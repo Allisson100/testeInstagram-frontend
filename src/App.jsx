1bae6609f4995f6324b9bb7bad1cb5b5ebf256ab
Allisson100 <allisson.boiane@outlook.com>
2024-07-09 12:17:38 -0300

    hhehe

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-// import { LoginSocialFacebook } from "reactjs-social-login";
-// import { FacebookLoginButton } from "react-social-login-buttons";
+import { LoginSocialFacebook } from "reactjs-social-login";
+import { FacebookLoginButton } from "react-social-login-buttons";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -30,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      {/* <LoginSocialFacebook
+      <LoginSocialFacebook
         appId="2732183950278210"
         onResolve={({ provider, data }) => {
           console.log("provider", provider);
@@ -41,7 +41,7 @@ function App() {
         }}
       >
         <FacebookLoginButton />
-      </LoginSocialFacebook> */}
+      </LoginSocialFacebook>
     </>
   );
 }
